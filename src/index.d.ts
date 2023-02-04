8ab03a2c40467144d583390fe6c20ec189daa55a
merylldindin <merylldin@gmail.com>
2023-02-04 14:51:23 -0800

    feat: add typing for vuetify design plugin

---
+++ src/index.d.ts
@@ -0,0 +1,17 @@
+import { ColorName } from "@/plugins/vuetify.client/colors";
+import { IconName } from "@/plugins/vuetify.client/icons";
+
+declare module "#app" {
+  interface NuxtApp {
+    $COLOR: ColorName;
+    $ICON: Record<IconName, string>;
+  }
+}
+
+declare module "vue" {
+  interface ComponentCustomProperties {
+    $COLOR: ColorName;
+    $ICON: Record<IconName, string>;
+  }
+}
+export {};
