9932df8b433b64fbbcaceb0256e2f7906577b015
merylldindin <merylldin@gmail.com>
2023-02-22 18:04:11 -0800

    feat: enhance schema for person

--- src/content/schema.ts
+++ src/content/schema.ts
@@ -1,6 +1,15 @@
 export const DEFAULT_IDENTITY = {
+  address: {
+    "@type": "PostalAddress",
+    addressCountry: "United States",
+    addressLocality: "Santa Monica",
+    addressRegion: "California",
+    postalCode: "90401",
+  },
   image: "https://cdn.merylldindin.com/assets/avatar.webp",
+  jobTitle: "Chief Technology Officer",
   name: "Meryll Dindin",
+  nationality: "France",
   sameAs: [
     "https://github.com/merylldindin",
     "https://www.linkedin.com/in/merylldindin/",
@@ -9,5 +18,23 @@ export const DEFAULT_IDENTITY = {
     "https://www.instagram.com/merylldindin/",
     "https://www.facebook.com/merylldindin",
     "https://dindinmeryll.portfoliobox.net",
+  ],
+  url: "https://merylldindin.com",
+  worksFor: [
+    {
+      "@type": "Organization",
+      name: "Polygon Technologies, Inc.",
+      url: "https://hellopolygon.com",
+    },
+    {
+      "@type": "Organization",
+      name: "Dillygence SAS",
+      url: "https://dillygence.com",
+    },
+    {
+      "@type": "Organization",
+      name: "Auberge de Cercoux SAS",
+      url: "https://auberge.decercoux.com",
+    },
   ],
 };
