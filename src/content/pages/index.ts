69a5b8ce95ae16f9061e5421e7ba2cb7a3257fb7
merylldindin <merylldin@gmail.com>
2023-06-13 20:07:13 -0600

    feat: integrate page for fujitsu

--- src/content/pages/index.ts
+++ src/content/pages/index.ts
@@ -1,6 +1,7 @@
 export { default as AUBERGE_PAGE_CONTENT } from "./AUBERGE_PAGE.json";
 export { default as CALASTER_PAGE_CONTENT } from "./CALASTER_PAGE.json";
 export { default as DILLYGENCE_PAGE_CONTENT } from "./DILLYGENCE_PAGE.json";
+export { default as FUJITSU_PAGE_CONTENT } from "./FUJITSU_PAGE.json";
 export { default as HUSTLE_PAGE_CONTENT } from "./HUSTLE_PAGE.json";
 export { default as LANDING_PAGE_CONTENT } from "./LANDING_PAGE.json";
 export { default as POLYGON_PAGE_CONTENT } from "./POLYGON_PAGE.json";
--- src/types/routes.ts
+++ src/types/routes.ts
@@ -2,6 +2,7 @@ export enum RoutePath {
   AUBERGE_DE_CERCOUX = "/hustle/auberge-de-cercoux/",
   CALASTER = "/hustle/calaster/",
   DILLYGENCE = "/hustle/dillygence/",
+  FUJITSU = "/hustle/fujitsu/",
   HUSTLE = "/hustle/",
   LANDING_PAGE = "/",
   POLYGON = "/hustle/polygon/",
