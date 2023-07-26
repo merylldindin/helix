b12c4b7e0de20439f1a0c4af347e07ae56cd80cf
merylldindin <merylldin@gmail.com>
2023-07-25 17:57:00 -0700

    feat: add missions page

---
+++ src/components/pages/components/FrameSection.vue
@@ -0,0 +1,73 @@
+<script lang="ts" setup>
+import { PropType } from "vue";
+import { useDisplay } from "vuetify";
+
+import { CustomHeadline, CustomImage, CustomSection } from "@/components/shared";
+import { DeliveredImage } from "@/types";
+
+defineProps({
+  image: {
+    required: true,
+    type: Object as PropType<DeliveredImage>,
+  },
+  title: {
+    required: true,
+    type: String,
+  },
+});
+
+const { smAndDown } = useDisplay();
+</script>
+
+<template>
+  <CustomSection fullscreen snap>
+    <template #background>
+      <CustomImage class="image-wrapper custom-shadow" cover :image="image" />
+    </template>
+
+    <div class="title-wrapper">
+      <CustomHeadline
+        class="headline-wrapper custom-shadow"
+        :level="2"
+        :text="title"
+        :typography="`text-foam text-cartesian title-typography ${
+          smAndDown ? 'headline-3' : 'headline-2'
+        }`"
+      />
+    </div>
+  </CustomSection>
+</template>
+
+<style lang="scss" scoped>
+.image-wrapper {
+  height: calc(100% - 112px);
+  max-width: calc(100vw - 64px);
+  margin: 80px 32px 32px;
+  opacity: 1;
+
+  @include sm-down {
+    height: calc(100% - 80px);
+    max-width: calc(100% - 32px);
+    margin: 64px 16px 16px;
+  }
+}
+
+.title-wrapper {
+  height: 100%;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+
+.headline-wrapper {
+  padding: 2rem 4rem;
+  background-color: rgb(0 0 0 / 33%);
+  border-radius: 10px;
+
+  @include sm-down {
+    padding: 1.5rem 3rem;
+    border-radius: 0 10px 10px 0;
+  }
+}
+</style>
--- src/components/pages/utils.ts
+++ src/components/pages/utils.ts
@@ -1,6 +1,7 @@
 import { ComponentName } from "@/types";
 
 import ArticleSection from "./components/ArticleSection.vue";
+import FrameSection from "./components/FrameSection.vue";
 import ImageSection from "./components/ImageSection.vue";
 import LegalSection from "./components/LegalSection.vue";
 import RotatingCube from "./components/RotatingCube/main.vue";
@@ -8,6 +9,7 @@ import TimelineSection from "./components/TimelineSection/main.vue";
 
 export const ASYNC_COMPONENTS: Record<string, any> = {
   [ComponentName.ARTICLE_SECTION]: ArticleSection,
+  [ComponentName.FRAME_SECTION]: FrameSection,
   [ComponentName.IMAGE_SECTION]: ImageSection,
   [ComponentName.LEGAL_SECTION]: LegalSection,
   [ComponentName.ROTATING_CUBE]: RotatingCube,
