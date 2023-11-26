0a5b16693bbfb0bfd5af65a00e4febf2cf4906c3
merylldindin <merylldin@gmail.com>
2023-11-26 21:51:12 +0100

    feat: introduce suggestions links

---
+++ src/content/pages/suggestions/index.ts
@@ -0,0 +1,5 @@
+export { default as BOOK_SUGGESTIONS } from "./BOOK_SUGGESTIONS.json";
+export { default as ENHANCER_SUGGESTIONS } from "./ENHANCER_SUGGESTIONS.json";
+export { default as NEWSLETTER_SUGGESTIONS } from "./NEWSLETTER_SUGGESTIONS.json";
+export { default as PODCAST_SUGGESTIONS } from "./PODCAST_SUGGESTIONS.json";
+export { default as TOOLKIT_SUGGESTIONS } from "./TOOLKIT_SUGGESTIONS.json";
